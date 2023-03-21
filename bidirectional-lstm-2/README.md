1f1abad62a62e589e26a2b5216bb9a5dc62d914f
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2023-03-21 19:11:10 -0400

    Add a second memory-friendly bidirectional example

---
+++ bidirectional-lstm-2/README.md
@@ -0,0 +1,3 @@
+# Classification using LSTM
+
+In this example I'm using a bidirectional LSTM and a [GRU](https://www.tensorflow.org/api_docs/python/tf/keras/layers/GRU?hl=en) (gated recurrent unit) and an additional Dense layer. I've tweaked the batch size, learning rate, and some more things and ensure that the validation accuracy is increasing consistently.
---
+++ bidirectional-lstm-2/train.py
@@ -0,0 +1,100 @@
+import json
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.layers import Dense, LSTM, Conv1D, Embedding, MaxPooling1D, \
+                         Bidirectional, BatchNormalizationV2, PReLU, GlobalAveragePooling1D, GRUV2
+from keras.utils import to_categorical, pad_sequences
+from keras.optimizers import Adam
+from keras import Input, Model
+from math import floor
+
+# This constant represents the shape of the output. Since we have three different states (negative,
+# neutral, positive) we want to instruct our neural network to give us probabilities for just those.
+SHAPE = 3
+
+# Extract data from a tsv. We don't need to do anything with the first row. You can limit the amount
+# of data (for testing your model on smaller datasets) by adding `max_rows=X`, where X a positive
+# number.
+training = np.genfromtxt(
+    '../data/out.tsv', delimiter='\t', skip_header=1,
+    usecols=(0, 1), dtype=None)
+
+# The first column contains all the Tweets. Make sure that these are cleaned of special characters.
+train_x = [x[0].lower().strip() for x in training]
+
+# The second column contains all the labels.
+train_y = np.asarray([x[1] for x in training])
+
+# In this example I'll test with more words than the dense example.
+max_words = 8000
+max_text_len = 140
+
+# The tokenizer will be used to tokenize the tweets and index all words that were found in the entire
+# corpus/dataset.
+tokenizer = Tokenizer(num_words=max_words)
+train_x = [str(x) for x in train_x]
+
+# This line fits the tokenizer on all our text.
+tokenizer.fit_on_texts(train_x)
+
+# Once fitted, the tokenizer instance has this `word_index` member which contains the dict to all of
+# the words in the dataset.
+dictionary = tokenizer.word_index
+
+# Save the tokenizer so that it can be loaded in the predictor. I don't save the dictionary, as I do
+# not use it at all.
+with open('tokenizer.json', 'w') as tokenizer_file:
+    json.dump(json.loads(tokenizer.to_json()), tokenizer_file)
+
+# Here I create sequences from the text, based on the dictionary. Then I pad all sequences to the max
+# allowed length of text.
+train_x = tokenizer.texts_to_sequences(train_x)
+train_x = pad_sequences(train_x, maxlen=max_text_len)
+
+# Like previously mentioned, we need to do something similar with the labels. So we create categories.
+train_y = to_categorical(train_y, SHAPE)
+
+# I follow the exact same pattern as the `dense` example.
+inputs = Input(shape=(max_text_len,))
+
+# I changed the strategy here a little bit and used two `Bidirectional(LSTM(X))` layers. This method
+# is memory-expensive and requires more epochs to train.
+out = Embedding(max_words, 64, input_length=max_text_len)(inputs)
+out = Conv1D(filters=64, kernel_size=3, padding='same', activation='relu')(out)
+out = MaxPooling1D(pool_size=2)(out)
+out = Bidirectional(LSTM(96, return_sequences=True, activation='softsign'))(out)
+out = GRUV2(96)(out)
+# The following also works instead of the above.
+# out = GlobalAveragePooling1D()(out)
+out = Dense(64, activation='relu')(out)
+out = PReLU()(out)
+out = Dense(32, activation='relu')(out)
+
+# The final layer gives us the output with the desired shape: An array of three predictions.
+out = Dense(SHAPE, activation='softmax')(out)
+
+model = Model(inputs, [out])
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(learning_rate=1e-4),
+              metrics=['accuracy'])
+
+train_amount = floor(len(train_x) * 0.7)
+
+# Like in the `dense` example, you can try tweaking the settings here and observe the effects.
+model.fit(train_x[:train_amount], train_y[:train_amount],
+          batch_size=64,
+          epochs=20,
+          verbose=1, # type: ignore
+          validation_data=(train_x[-train_amount + 1:], train_y[-train_amount + 1:]),
+          shuffle=True)
+
+# I save the model and the weights here so that I can deploy it and reuse it as I see fit.
+model_json = model.to_json()
+
+with open('classifier-model.json', 'w') as json_file:
+    json_file.write(model_json)
+
+model.save_weights('classifier-weights.h5')
+model.save('classifier-model.h5')
+
+print('Training done')
